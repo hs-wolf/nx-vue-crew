5dadcf33caa32c7dffd3d235205030b745240020
Josh Israel <uno.braid@gmail.com>
2022-07-05 17:34:13 -0300

    Created an empty Nx plugin workspace.

---
+++ e2e/nx-vue-crew-e2e/tests/nx-vue-crew.spec.ts
@@ -0,0 +1,56 @@
+import {
+  checkFilesExist,
+  ensureNxProject,
+  readJson,
+  runNxCommandAsync,
+  uniq,
+} from '@nrwl/nx-plugin/testing';
+
+describe('nx-vue-crew e2e', () => {
+  // Setting up individual workspaces per
+  // test can cause e2e runs to take a long time.
+  // For this reason, we recommend each suite only
+  // consumes 1 workspace. The tests should each operate
+  // on a unique project in the workspace, such that they
+  // are not dependant on one another.
+  beforeAll(() => {
+    ensureNxProject('nx-vue-crew', 'dist/packages/nx-vue-crew');
+  });
+
+  afterAll(() => {
+    // `nx reset` kills the daemon, and performs
+    // some work which can help clean up e2e leftovers
+    runNxCommandAsync('reset');
+  });
+
+  it('should create nx-vue-crew', async () => {
+    const project = uniq('nx-vue-crew');
+    await runNxCommandAsync(`generate nx-vue-crew:nx-vue-crew ${project}`);
+    const result = await runNxCommandAsync(`build ${project}`);
+    expect(result.stdout).toContain('Executor ran');
+  }, 120000);
+
+  describe('--directory', () => {
+    it('should create src in the specified directory', async () => {
+      const project = uniq('nx-vue-crew');
+      await runNxCommandAsync(
+        `generate nx-vue-crew:nx-vue-crew ${project} --directory subdir`
+      );
+      expect(() =>
+        checkFilesExist(`libs/subdir/${project}/src/index.ts`)
+      ).not.toThrow();
+    }, 120000);
+  });
+
+  describe('--tags', () => {
+    it('should add tags to the project', async () => {
+      const projectName = uniq('nx-vue-crew');
+      ensureNxProject('nx-vue-crew', 'dist/packages/nx-vue-crew');
+      await runNxCommandAsync(
+        `generate nx-vue-crew:nx-vue-crew ${projectName} --tags e2etag,e2ePackage`
+      );
+      const project = readJson(`libs/${projectName}/project.json`);
+      expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
+    }, 120000);
+  });
+});
---
+++ packages/nx-vue-crew/src/generators/nx-vue-crew/schema.d.ts
@@ -0,0 +1,5 @@
+export interface NxVueCrewGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
