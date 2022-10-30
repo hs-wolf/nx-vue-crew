94e04b524d81d44246cf96611338ab0e170ccb25
Josh Israel <uno.braid@gmail.com>
2022-10-30 18:22:56 -0300

    Created a Nuxt3 SSR app and configured it for the Nx monorepo.

---
+++ apps/nuxt-app/nuxt.config.ts
@@ -0,0 +1,24 @@
+export default defineNuxtConfig({
+  typescript: { shim: false },
+  nitro: {
+    output: {
+      dir: '~~/../../dist/apps/nuxt-app',
+    },
+  },
+  srcDir: 'src/',
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      title: 'nuxt-app',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'nuxt-app-description' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+  css: ['~/assets/css/global.css'],
+});
